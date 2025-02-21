38022f548c254d0bc0f7bb6c4dd779c8734fadea
danielles0xg <daniel.profile.eng@gmail.com>
2025-02-21 09:42:42 -0500

    add client to paht

--- src/pages/api/route.ts
+++ src/pages/api/route.ts
@@ -8,9 +8,8 @@ type ResponseData = {
 export default async function handler(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
   try {
     const { action, address, publisher, winnerId } = JSON.parse(JSON.stringify(req.body));
-    console.log('action::', process.env.PRISM_SDK_API_KEY);
     let responseData = null;
-    const prismClient = new PrismClient('60f5388cb532c14b2fdd2e481ad45234326702037711b0324f629c816081d73f');
+    const prismClient = new PrismClient(process.env.PRISM_SDK_API_KEY!);
     switch (action) {
       case 'auction': {
         responseData = await prismClient.triggerAuction(address, publisher);
