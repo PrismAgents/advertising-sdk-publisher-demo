c8a34d7b2b7f3eb199fd4f9398836b3b4fc6a4e5
danielles0xg <daniel.profile.eng@gmail.com>
2025-02-21 09:05:58 -0500

    update sdk prod

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // basePath: '/client',
   reactStrictMode: true,
   webpack: (config:any) => {
     config.externals.push('pino-pretty', 'lokijs', 'encoding')
