4abdbfbc418dd492613bcb9d23c253d8d2d6ad1a
danielles0xg <daniel.profile.eng@gmail.com>
2025-07-22 05:47:20 -0400

    added bepolia custom chain to wagmi

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,4 +1,4 @@
-import { getDefaultConfig } from '@rainbow-me/rainbowkit';
+import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import {
   arbitrum,
   base,
@@ -6,14 +6,25 @@ import {
   optimism,
   polygon,
   sepolia,
-  arbitrumSepolia
-} from 'wagmi/chains';
+  arbitrumSepolia,
+} from "wagmi/chains";
+import { type Chain } from "viem";
 
-export const  wagmiConfig = getDefaultConfig({
-  appName: 'Prism-Tint Frontend',
-  projectId: 'a7649e735f89fc5e232cecc7fa1d0675',
-  chains: [
-    arbitrumSepolia,
-  ],
+export const BerachainBepolia = {
+  id: 80069,
+  name: "Berachain Bepolia",
+  nativeCurrency: { name: "BERA", symbol: "BERA", decimals: 18 },
+  rpcUrls: {
+    default: { http: ["https://bepolia.rpc.berachain.com"] },
+  },
+  blockExplorers: {
+    default: { name: "Bepolia Block Explorer", url: "https://bepolia.beratrail.io/" },
+  }
+} as const satisfies Chain;
+
+export const wagmiConfig = getDefaultConfig({
+  appName: "Prism-Tint Frontend",
+  projectId: "a7649e735f89fc5e232cecc7fa1d0675",
+  chains: [BerachainBepolia],
   ssr: true,
 });
